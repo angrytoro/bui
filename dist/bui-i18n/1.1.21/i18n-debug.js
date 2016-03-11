426dd87a09affa6a8fe164225528843fed9c89e3
h261080299 <835053230@qq.com>
2016-03-11 16:42:48 +0800

    bui 国际化英文文案文档整理

--- dist/bui-i18n/1.1.21/i18n-debug.js
+++ dist/bui-i18n/1.1.21/i18n-debug.js
@@ -1,7 +1,7 @@
 (function() {
 	var i18n = {
 		'zh_CN': {
-			cancel : "取消",
+
 			inputDifferent  : "两次输入不一致！",
 			notNull : "不能为空！",
 			inputNotLess : "输入值不能小于{0}！",
@@ -45,6 +45,7 @@
 			next : "下一页",
 			LastPage : "末 页",
 			ensure : "确定",
+            cancel : "取消",
 			totalPage : "共 {totalPage} 页",
 			noPageA : '第 <input type="text"',
 			noPageB : 'pb-page" size="20" value="{curPage}" name="inputItem"> 页',
@@ -62,6 +63,69 @@
 			pageQuery : "本页合计"
 		}
 	};
+    i18n.en = {
+
+        inputDifferent:"The value entered does not match previous one!",
+        notNull:"Cannot be empty!",
+        inputNotLess:"The value entered does not be less than {0}!",
+        inputNotMore:"The value entered does not be more than {0}!",
+        inputLenght:"Length of  value entered is {0}!",
+        inputLenNotLess:"Minimum length is {0}!",
+        inputLenNotMore:"Maximum length is {0}!",
+        inputIllegal:"Invaild {0} format!",
+        mailIllegal:"Invaild email address!",
+        dateIllegal:"Invaild date!",
+        dateNotLess : "The date entered cannot be earlier than {0}!",
+        dateNotMore:"The date entered cannot be later than {0}!",
+        dateInputError:"The start date cannot be later than the end date!",
+        phoneIllegal:"Invaild moblie!",
+        numberIllegal:"Invaild number!",
+        numberInputError:"The initial number cannot be greater than the end number!",
+        mustSelectOption:"At least {0} option(s) must be checked!",
+        pleaseSelect:"Choose one...",
+        uploadFileError:"Error uploading file!",
+        loading:"Submitting...",
+        startNotMoreEnd:"The start time cannot be later than the end time!",
+        ascending:"Asc",
+        descending:"Desc",
+        setColumn:"Set column",
+        autoSize:"Auto size",
+        actualSize:"Actual size",
+        fitSize:"Fit size",
+        leftHand:"Anticlockwise",
+        rightHand:"Clockwise",
+        zoom:"Zoom in",
+        micrify:"Zoom out",
+        viewImg:"View image",
+        close:"Close",
+        closeOther:"Close other",
+        closeAll:"Close all",
+        title:"Title",
+        inputTag:"Input Tag",
+        refresh:"Refresh",
+        homePage:"First",
+        prev:"Previous",
+        next:"Next",
+        LastPage:"Last",
+        ensure:"Confirm",
+        cancel:"Cancel",
+        totalPage:"{totalPage} Pages",
+        noPageA:"Page <input type='text'",
+        noPageB: 'pb-page" size="20" value="{curPage}" name="inputItem"> ',
+        totalRecord:"{totalCount}  Records",
+        delBtn:"Delete",
+        uploadFile:"Upload",
+        paramError:"Parameter Error!",
+        serverInput:"Output of server:",
+        objNull:"Object is null!",
+        uploadCount:"Bytes uploaded:",
+        fileIllegal:"Missing crossdomain.xml file or the file is not legal!",
+        fileIllegal2:"Missing crossdomain.xml file or the file is not legal!",
+        serverReturnErr:"The data returned by the server is incorrect!",
+        totalQuery:"Query totals",
+        pageQuery:"Current page totals"
+    };
+
 	var current;
 
 	if(typeof window.locale === 'string') {
@@ -69,7 +133,7 @@
 	} else {
 		current = i18n['zh_CN']; //默认是中文
 	}
-	
+
 	/**
 	 * 根据传入的key获取相应的key对应的翻译
 	 * @param  {[type]} key [description]
